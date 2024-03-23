166794b8d0a7b7554c11f5c02b45a007070cdaa4
Your Name <you@example.com>
2024-03-23 02:23:58 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -100,7 +100,13 @@ Puedes usar una pila auxiliar.
 
 void copia_pila(Stack* P1, Stack* P2)
 {
-
+  Stack *P3 = createSrack();
+  void *dato;
+  while ((dato = pop(P1)))
+    {
+      push(P3,dato);
+      
+    }
 }
 
 /*
